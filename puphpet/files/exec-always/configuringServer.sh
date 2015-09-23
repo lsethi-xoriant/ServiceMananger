7a9963e19900a9cd9056609c09ae08bb693ccf81
Adnan Mulalic <stensolino@hotmail.com>
2015-09-23 11:34:08 +0200

    Fix problem with isntalling node

--- puphpet/files/exec-always/configuringServer.sh
+++
@@ -1,6 +0,0 @@
-#This will be executed on initial $vagrant up and all $vagrant provision
-#touch /var/www/serviceapi/initialUp
-cp /var/www/puphpet/customConf/serviceapi.conf /etc/nginx/sites-available/serviceapi.conf
-service nginx restart
-cd /var/www/serviceapi
-bundle exec unicorn -c /var/www/puphpet/customConf/unicorn.rb -D
