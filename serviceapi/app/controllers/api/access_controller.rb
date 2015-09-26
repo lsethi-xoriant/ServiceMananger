bc0cd007ad6c96ea37cb9135238e05dc88328c00
Osman Perviz <osmanperviz@outlook.com>
2015-09-26 14:22:38 +0000

    add auth_token column to users table,create login&logout methods in access_ctrl,add current_user and checking token funcionalty

--- serviceapi/app/controllers/api/access_controller.rb
+++ serviceapi/app/controllers/api/access_controller.rb
@@ -1,11 +1,29 @@
 class Api::AccessController < ApplicationController
 
   def create
-
+    @user = User.where(:email=>params[:access][:email]).first
+    if @user && @user.authenticate(params[:access][:password])
+      token = generate_new_token
+      @user.update_attributes(:auth_token => token)
+      render json: @user,:except=>[:password_digest],status: 200
+    else
+      render json:{message:"Bad Credentials"},status:401
+    end
   end
 
   def destroy
-    
+    user = User.find(params[:id])
+    if user && user.update_column(:auth_token,nil)
+      render json:{message:"Successfully Logout"},status: :ok
+    else
+      render json: {error:user.errors},status: 404
+    end
+  end
+
+  private
+
+  def generate_new_token
+    SecureRandom.uuid.gsub(/\-/,'')
   end
 
 end
--- serviceapi/app/controllers/application_controller.rb
+++ serviceapi/app/controllers/application_controller.rb
@@ -2,4 +2,30 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+
+  helper_method :current_user
+
+  def restrict_access
+    authenticate_token || render_unauthorize
+  end
+
+
+  def authenticate_token
+    authenticate_with_http_token do |token,options|
+      User.find_by(:auth_token => token)
+    end
+  end
+
+  def render_unauthorize
+    self.headers['WWW-Authenticate'] = 'Token realm = "Application"'
+    render json:{message:"You Are Not Login!"},status: 401
+  end
+
+  def current_user
+    authenticate_token
+  end
+
+
+
 end
