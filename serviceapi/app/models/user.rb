68f96d48bc193da92704c31368be187dd69d3725
osman <osmanperviz@gmail.com>
2015-10-08 23:57:22 +0200

    setrelation ship user -> permissions througt=> groups

--- serviceapi/app/models/user.rb
+++ serviceapi/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   belongs_to :group
+  has_many :permissions,:through => :group
 
   VALID_EMAIL = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email,
