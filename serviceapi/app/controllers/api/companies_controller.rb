16e088aae1fea59cb61f7c7ef4e9e0ae9610c181
osman <osmanperviz@gmail.com>
2015-10-09 13:44:45 +0200

    create Company CRUD actions in company_controller

--- serviceapi/app/controllers/api/companies_controller.rb
+++ serviceapi/app/controllers/api/companies_controller.rb
@@ -1,2 +1,50 @@
 class Api::CompaniesController < ApplicationController
+
+  def index
+    @companies = Company.all
+    render json: @companies
+  end
+
+  def create
+    @company = Company.create(company_params)
+    if @company.save
+      render json: @company,status: 201
+    else
+      render json: @company.errors,status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+    render json: @company
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update_attributes(user_params)
+      render json: @company,status: 200
+    else
+      render json: @company.errors,status: 404
+    end
+  end
+
+
+  def destroy
+    @company = Company.find(params[:id])
+    if @company.destroy
+      render nothing:true,status: 200
+    else
+      render json: @company.errors
+    end
+  end
+
+
+
+  private
+
+  def company_params
+    params.require(:company).permit(:email,:name,:city,:country,:user_id)
+  end
+
+
 end
