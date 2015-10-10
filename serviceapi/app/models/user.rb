8485166e700cc0d5d33ed08098df1c265ff3a561
osman <osmanperviz@gmail.com>
2015-10-10 08:57:21 +0200

    set relationship many to many company->users,create join table

--- serviceapi/app/models/user.rb
+++ serviceapi/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   belongs_to :group
+  has_and_belongs_to_many :companies
   has_many :permissions,:through => :group
 
   VALID_EMAIL = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
