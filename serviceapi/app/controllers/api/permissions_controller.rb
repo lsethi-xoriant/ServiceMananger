dddceb11c5b936e5e2cbee778fea4c4a0a73b9c9
osman <osmanperviz@gmail.com>
2015-11-27 13:40:39 +0100

    re #117 refacture permission json output

--- serviceapi/app/controllers/api/permissions_controller.rb
+++ serviceapi/app/controllers/api/permissions_controller.rb
@@ -1,8 +1,11 @@
 class Api::PermissionsController < ApplicationController
+  respond_to :json
+
   load_and_authorize_resource
+
   def index
     @permissions = Permission.all
-    render json: @permissions
+    respond_with @permissions,status: 200
   end
 
 end
