66eec1650c0beaebb4f78dabdbb220eb561d7d65
osman <osmanperviz@gmail.com>
2015-11-27 12:19:32 +0100

    re #117 refacture user json output,send user with all nested objects

--- serviceapi/app/controllers/api/users_controller.rb
+++ serviceapi/app/controllers/api/users_controller.rb
@@ -1,35 +1,38 @@
 class Api::UsersController < ApplicationController
   include ReusableMethods
+  respond_to :json
+
   load_and_authorize_resource
   before_filter :check_params,only:[:create,:update]
 
 
   def index
     @users = User.accessible_by(current_ability)
-    render json: @users,:except=>[:password_digest],:include=>[:groups=>{:only=>:name}]
+    respond_with @users,status: 200
   end
 
   def create
     @user = User.new(user_params)
     @user.validation_trigger = true
     if @user.save
-      render json: @user,status: 201
+      respond_with @user,status: 201
     else
-      render json: @user.errors,status: :unprocessable_entity
+      respond_with @user.errors,status: :unprocessable_entity
     end
   end
 
   def show
     @user = User.find(params[:id])
-    render json: @user,:except=>[:password_digest],:include=>[:groups=>{:only=>[:name,:id]}]
+    respond_with @user,status:200
   end
 
   def update
     @user = User.find(params[:id])
+    @user.validation_trigger = true
     if @user.update_attributes(user_params)
-      render json: @user,status: 200
+      respond_with @user,status: 200
     else
-      render json: @user.errors,status: 404
+     render json: @user.errors,status: :unprocessable_entity
     end
   end
 
@@ -55,12 +58,6 @@ private
     checking_ids(current_user.companies.pluck(:id),params[:user][:company_ids]) if params[:user][:company_ids].present?
     check_group_ids(params[:user][:group_ids]) if params[:user][:group_ids].present?
     check_store_ids(params[:user][:store_ids]) if params[:user][:store_ids].present?
-  end
-
-  def checking_existence_company_id?
-    unless params[:user][:company_id]
-
-    end
   end
 
 
