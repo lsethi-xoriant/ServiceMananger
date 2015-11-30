eac7933c316feda58b5cb61ae7308204da1204eb
osman <osmanperviz@gmail.com>
2015-12-01 00:04:34 +0100

    fix error 500 on  access_ctrl

--- serviceapi/app/controllers/application_controller.rb
+++ serviceapi/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :restrict_access
-
+  before_action :set_locale
   check_authorization
 
 
