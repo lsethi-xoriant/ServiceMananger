f86fb98b5765658d422ddba276cbac924c77a135
Adnan Mulalic <stensolino@hotmail.com>
2015-12-02 23:08:11 +0100

    re #128 Add company controller and view

---
+++ serviceadmin/scripts/controllers/companyController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    function companyController($scope, $http, $location, currentUserServices) {
+        if (!currentUserServices.loggedIn()) {
+            $location.path("/login");
+            return;
+        }
+
+        $scope.breadcrumb = currentUserServices.getBreadcrumb(breadcrumbConst.company);
+        $scope.userData = currentUserServices.getProfile();
+    }
+
+    managernoControllers.controller("companyController", companyController);
+})();
