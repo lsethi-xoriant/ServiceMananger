06670f364c6cace90d644bb918bc6df51e76d05a
osman <osmanperviz@gmail.com>
2016-01-02 23:41:59 +0100

    re #135 add default no-image path to companie resource

--- serviceapi/app/models/company.rb
+++ serviceapi/app/models/company.rb
@@ -3,20 +3,27 @@ class Company < ActiveRecord::Base
   has_many :stores
   has_many :groups,:through => :stores
 
+  before_save :check_image
 
   VALID_EMAIL = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
-  validates :email,
-            :presence => true,
-            :format => VALID_EMAIL,
-            :uniqueness => true
-
-  validates :name,
-            :presence => true,
-            :uniqueness => true
-
-  validates :city,
-            :presence => true
+  # validates :email,
+  #           :presence => true,
+  #           :format => VALID_EMAIL,
+  #           :uniqueness => true
+  #
+  # validates :name,
+  #           :presence => true,
+  #           :uniqueness => true
+  #
+  # validates :city,
+  #           :presence => true
 
   validates :country,
             :presence => true
+
+
+
+  def check_image
+    self.logoImageLink = NO_IMAGE_LINK unless self.logoImageLink
+  end
 end
